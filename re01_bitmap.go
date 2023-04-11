14270d647e36561df3cb1bc04e03e9c4002d1f89
gnc40yoshikawa <45777930+gnc40yoshikawa@users.noreply.github.com>
2023-04-12 01:02:46 +0900

    ビットマスクの作成 アルファベットのインデックス取得機能

--- re01_bitmap.go
+++ re01_bitmap.go
@@ -1,10 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	Text := [...]string{"A", "A", "C", "B", "B", "A", "A", "C", "A", "B", "A", "B", "A", "B"}
-	Pat := [...]string{"A", "C", "A", "B", "A", "B"}
+	//対象文字列
+	var Text = []string{"A", "A", "C", "B", "B", "A", "A", "C", "A", "B", "A", "B", "A", "B"}
+	//検索文字列
+	var Pat = []string{"A", "C", "A", "B", "A", "B"}
+	//ビットマスク
+	var Mask [26]int
 	fmt.Println(Text)
 	fmt.Println(Pat)
+
+	GenerateBitMask(Pat, Mask)
+}
+
+func GenerateBitMask(Pat []string, Mask [26]int) {
+	//検索文字列の要素数を取得
+	var PatLen int = len(Pat)
+	//ビットマスクを初期化
+	for i := 0; i < 26; i++ {
+		Mask[i] = 0b0
+	}
+	fmt.Printf("%016b\n", Mask)
+
+	for i := 0; i < PatLen; i++ {
+		//何番目のアルファベットに位置するか
+		fmt.Println(Index(Pat[i]))
+	}
+}
+
+func Index(s string) any {
+	var obj string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	return strings.Index(obj, s)
 }
